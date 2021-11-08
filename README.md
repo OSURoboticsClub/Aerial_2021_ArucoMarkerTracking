e83f340e4c454aed0d75d8a13d8a28309b8169b3
Nathan Rumsey <nathan.s.rumsey@gmail.com>
2021-11-07 17:37:55 -0800

    Prepare for first release

--- README.md
+++ README.md
@@ -14,20 +14,4 @@
 []
 
 # Troubleshooting
-[]# Aruco Marker Tracking Package
-[Description Here]
-
-# Contents
-* 1
-* 2
-* 3
-
-# FAQ
-## How to install and use?
-[]
-
-## Camera Calibration?
-[]
-
-# Troubleshooting
 []
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-description = ""
+description = "Contains classes and functions to handle tracking of Aruco markers using OpenCV."
 
 setuptools.setup(
     name="ArucoMarkerTracking",
@@ -13,9 +13,9 @@ setuptools.setup(
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/OSURoboticsClub/", #! Real repo name
+    url="https://github.com/OSURoboticsClub/Aerial_2021_ArucoMarkerTracking",
     project_urls= {
-        "Issues": "https://github.com/OSURoboticsClub/[]/issues" #! real repo name
+        "Issues": "https://github.com/OSURoboticsClub/Aerial_2021_ArucoMarkerTracking/issues" 
     },
     packages=['ArucoMarkerTracking'],
     install_requires=['opencv-contrib-python', 'numpy'],
